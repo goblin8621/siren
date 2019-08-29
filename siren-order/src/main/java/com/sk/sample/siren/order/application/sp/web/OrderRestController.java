0841b5b9ff76e5a66b15452c7480d6bccbf9e222
karma2481 <karma2481@gmail.com>
2019-08-29 17:44:01 +0900

    rest add

--- siren-order/src/main/java/com/sk/sample/siren/order/application/sp/web/OrderRestController.java
+++ siren-order/src/main/java/com/sk/sample/siren/order/application/sp/web/OrderRestController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sk.sample.siren.order.domain.model.Order;
-import com.sk.sample.siren.order.domain.repository.OrderRepository;
 import com.sk.sample.siren.order.domain.service.OrderService;
 
 @RestController
@@ -18,15 +16,11 @@ public class OrderRestController implements OrderService {
 	@Autowired
 	private OrderService orderService;
 	
-	@Autowired
-	private OrderRepository orderRepository;	
-
 	@Override
 	@PutMapping("/{id}/purchased")
 	public void purchase(@PathVariable("id") Long id) {
 		orderService.purchase(id);
 	}
-
 
 	@Override
 	@PostMapping("/order")
